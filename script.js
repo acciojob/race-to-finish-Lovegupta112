c6ccee6d977091d69079ead4582665c31ddf15a0
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-01 01:21:09 +0530

    added promises

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-window.promises = [];
+let resultDiv=document.getElementById('output');
+let prom1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("hello");
+    },1200);
+})
+let prom2=new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+        resolve("Hii");
+    },1800);
+})
+let prom3=new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+        resolve("hola");
+    },2500);
+})
+let prom4=new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+        resolve("Whatsup");
+    },3400);
+})
+let prom5=new Promise((resolve,reject)=>{
+ setTimeout(()=>{
+        resolve("heyya");
+    },4500);
+})
+window.promises = [prom1,prom2,prom3,prom4,prom5];
+
+let res= Promise.any([prom1,prom2,prom3,prom4,prom5]);
+res.then((greet)=>{
+    resultDiv.innerText=greet;
+})
+res.catch((error)=>{
+    console.log(error);
+})
 
 // Do not change the code above this
 // add your promises to the array `promises`
